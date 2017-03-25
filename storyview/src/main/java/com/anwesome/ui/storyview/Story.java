6440fbbfdfbcca6d6205d4450557accf774a6236
Anwesh43 <anweshthecool0@gmail.com>
2017-03-25 13:25:38 +0530

    Showing the storyview

--- storyview/src/main/java/com/anwesome/ui/storyview/Story.java
+++ storyview/src/main/java/com/anwesome/ui/storyview/Story.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.*;
 import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,7 @@ public class Story {
     private List<Status> statuses = new ArrayList<>();
     private Activity activity;
     private int currIndex = 0;
+    private StoryView storyView;
     private Story(Activity activity) {
         this.activity = activity;
     }
@@ -23,6 +25,12 @@ public class Story {
     }
     public void addStatus(Status status) {
         statuses.add(status);
+    }
+    public void show(Activity activity) {
+        if(storyView == null) {
+            storyView = new StoryView(activity);
+            activity.addContentView(storyView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        }
     }
     private class StoryView extends View{
         private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -40,6 +48,7 @@ public class Story {
             }
             try {
                 Thread.sleep(100);
+                invalidate();
             }
             catch (Exception ex) {
 
